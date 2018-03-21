fc30128ae4fb8a32604325e1e1ab666cb68ab1a7
quilam65 <quilt@atmarkcafe.org>
2018-03-21 13:57:01 +0700

    add rspec product controller(show, index, new, create)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,30 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+  before_action :assign_params, only: [:create]
   def index
     @products = Product.all
   end
+
+  def show
+
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(@product_params)
+    @product.save
+  end
+
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def assign_params
+      @product_params = params.require(:product).permit(:title, :description, :price, :category_id)
+    end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+
   describe '#index' do
     let!(:products) { create_list(:product, 5) }
     it 'gets a list of products' do
@@ -8,4 +9,42 @@ RSpec.describe ProductsController, type: :controller do
       expect(assigns(:products)).to eq products
     end
   end
+
+  describe '#new' do
+    let! (:product) { create(:product) }
+    it 'create a product' do
+      get :new
+      expect(assigns(:product)).to be_a_new(Product)
+    end
+  end
+
+
+  describe '#show' do
+    let! (:product) { create(:product) }
+    it "show product" do
+      get :show, params: { id: product.id }
+      # byebug
+      expect(assigns(:product)).to eq product
+    end
+  end
+
+  # describe '#edit' do
+  #   let! (:product) { create(:product) }
+  #   it "edit product" do
+  #     get :edit, params: { id: product.id }
+  #     # byebug
+  #     expect(assigns(:product)).to eq product
+  #   end
+  # end
+
+  describe '#create' do
+    it "creates a new product" do
+      expect{
+        post :create, params: { product: FactoryBot.attributes_for(:product) }
+      }.to change(Product,:count).by(1)
+    end
+  end
+
+
+
 end
