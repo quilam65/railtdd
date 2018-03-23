a1bd0c5b17b1d8b208f2332cb758acc8a3557fcd
quilam65 <quilt@atmarkcafe.org>
2018-03-23 16:41:39 +0700

    add tieng viet viet lu bu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/foxzi', as: 'rails_admin'
   resources :products
-  root 'products#index'
+
+  scope "(:locale)", :locale => /en|vn/ do
+    root "products#index"
+    get "products/index"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
